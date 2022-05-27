ac1c1a456e4b6799f7bd6038e2bca459c4a279f1
haltyy <sadeeyahmuhammad00@gmail.com>
2022-05-28 03:50:22 +0400

    last commit

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-//adding all the required modules.
+//list of added modules.
 const { request } = require('express');
 const express = require('express');
 var path = require("path");
 var fs = require("fs");
+const { ObjectId } = require('mongodb');
 const MongoClient = require('mongodb').MongoClient;
 
 const app = express();
@@ -10,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.set('port', 3000)
 
-//logger middleware. Logs all the incoming requests
+//logger middleware to output requests to server console
 app.use(function(req, res, next) {
     console.log("In comes a " + req.method + " request to " + req.url);
     next();
@@ -23,11 +24,10 @@ app.use((req, res, next) => {
 
 let db;
 
-//connection of the mongoclient
+//connecting mongodp compass
 MongoClient.connect('mongodb+srv://sadiya:sadiyamuhd@cluster0.xzza6.mongodb.net', (err, client) => {
     db = client.db('afterschoolclub');
 });
-
 
 app.get('/', (req, res, next) => {
     res.send('Select a collection, e.g., /collectionName');
@@ -38,7 +38,7 @@ app.param('collectionName', (req, res, next, collectionName) => {
     return next();
 });
 
-//GET request to view collections from MongoDB
+//mongodb GET request 
 app.get('/collection/:collectionName', (req, res, next) => {
     req.collection.find({}).toArray((e, results) => {
         if (e) return next(e)
@@ -46,10 +46,10 @@ app.get('/collection/:collectionName', (req, res, next) => {
     });
 });
 
-//GET request with a search query
+// mongodb GET request for search feature
 app.get('/collection/:collectionName/:k', (req, res) => {
     var key_1 = req.params.k;
-    console.log("Searched term: " + key_1);
+    console.log("Searched figure: " + key_1);
     
     req.collection.find({$or: [ {subject: { $regex: '^'+key_1, $options: "i" }}, {location: { $regex: '^'+key_1, $options: "i" }}]}).toArray((e, results) => {
         if (e) return console.log(e)
@@ -57,7 +57,7 @@ app.get('/collection/:collectionName/:k', (req, res) => {
     });
 });
 
-//POST request to add the order into the database
+//mongodb POST request 
 app.post('/collection/:collectionName', (req, res, next) => {
     req.collection.insertOne(req.body, (e, results) => {
         if (e) return next(e);
@@ -67,18 +67,18 @@ app.post('/collection/:collectionName', (req, res, next) => {
 
 const ObjectID = require('mongodb').ObjectID;
 
-//GET request to find a collection with a given ID
+//GET request for retrieving lesson by specifying the id
 app.get('/collection/:collectionName/:id', (req, res, next) => {
-req.collection.findOne({ _id: new ObjectID(req.params.id) }, (e, result) => {
+req.collection.findOne({ _id: new ObjectId(req.params.id) }, (e, result) => {
 if (e) return next(e)
 res.send(result)
 })
 })
 
-//PUT request to update the spaces of the lessons
+//PUT request to update the lessons
 app.put('/collection/:collectionName/:id', (req, res, next) => {
     req.collection.update(
-    {_id: new ObjectID(req.params.id)},
+    {_id: new ObjectId(req.params.id)},
     {$set: req.body},
     {safe: true, multi: false},
     (e, result) => {
@@ -110,7 +110,7 @@ app.put('/collection/:collectionName/:id', (req, res, next) => {
 //If an image does not exist in the static folder, the error message is displayed.
     app.use(function(req, res){
         res.status(404);
-        res.send("File not Found! try again...");
+        res.send("File not Found!");
     });   
 
 //deploying the server
